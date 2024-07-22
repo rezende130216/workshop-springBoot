68ef9f6347591bd87575d26e0328ea807acb8312
rezende130216 <ra-21157141-2@alunos.unicesumar.edu.br>
2024-07-22 11:26:18 -0300

    Add - Entidade pedido, datas com instant e padrão ISO 8601 PT-01

--- src/main/java/com/microservice/curso/entities/User.java
+++ src/main/java/com/microservice/curso/entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -21,6 +23,9 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @OneToMany(mappedBy = "client")
+    private final List<Order> orders = new ArrayList<>();
+
     public User() {
     }
 
@@ -34,6 +39,10 @@ public class User implements Serializable {
 
     public Long getId() {
         return id;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     public void setId(Long id) {
