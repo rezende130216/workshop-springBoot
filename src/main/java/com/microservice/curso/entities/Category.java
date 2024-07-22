783b9ff0b6e9c68039f92ed5e86de39b739c7758
rezende130216 <ra-21157141-2@alunos.unicesumar.edu.br>
2024-07-22 18:31:35 -0300

    Add - Associação muitos-para-muitos com JoinTable

--- src/main/java/com/microservice/curso/entities/Category.java
+++ src/main/java/com/microservice/curso/entities/Category.java
@@ -1,5 +1,6 @@
 package com.microservice.curso.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -21,7 +22,8 @@ public class Category implements Serializable {
 
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private final Set<Product> products = new HashSet<>();
 
     public Category() {
