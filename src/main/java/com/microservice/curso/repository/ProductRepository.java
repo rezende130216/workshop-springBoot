571da1af91443eb04ef8063a2b4d55fc8f5d0de8
rezende130216 <ra-21157141-2@alunos.unicesumar.edu.br>
2024-07-22 18:12:36 -0300

    Add - Entidade Product

---
+++ src/main/java/com/microservice/curso/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.microservice.curso.repository;
+
+import com.microservice.curso.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+}
---
+++ src/main/java/com/microservice/curso/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.microservice.curso.services;
+
+import com.microservice.curso.entities.Category;
+import com.microservice.curso.entities.Product;
+import com.microservice.curso.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAll(){
+        return productRepository.findAll();
+    }
+
+    public Product findById(Long id){
+        Optional<Product> obj = productRepository.findById(id);
+        return obj.get();
+    }
+}
