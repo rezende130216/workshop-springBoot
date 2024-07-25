a074f79ff67a6a1209bfb5f37e6c3f473496d8d1
rezende130216 <ra-21157141-2@alunos.unicesumar.edu.br>
2024-07-25 13:10:52 -0300

    Add- Tratamento de exceção - update

--- src/main/java/com/microservice/curso/services/UserService.java
+++ src/main/java/com/microservice/curso/services/UserService.java
@@ -4,6 +4,7 @@ import com.microservice.curso.entities.User;
 import com.microservice.curso.repository.UserRepository;
 import com.microservice.curso.services.exceptions.DataBaseException;
 import com.microservice.curso.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return  userRepository.save(entity);
+       try {
+           User entity = userRepository.getReferenceById(id);
+           updateData(entity, obj);
+           return  userRepository.save(entity);
+       }catch (EntityNotFoundException e){
+           throw new ResourceNotFoundException(id);
+       }
     }
 
     private void updateData(User entity, User obj) {
